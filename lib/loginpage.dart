35e1bcf78e7df66f84f6e731f2747fb42d6b26dd
bluehyena <42088479+bluehyena@users.noreply.github.com>
2020-12-10 13:54:18 +0900

    [Style] - Add obscureText

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -66,6 +66,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
               hintText: '비밀번호를 입력하세요',
             ),
+            obscureText: true,
           ),
         )
       ],
